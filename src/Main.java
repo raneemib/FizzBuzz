aa7bdca78271f086de4dfa5e4302337d98cd1406
raneemib <ramenib@gmail.com>
2014-04-23 17:18:58 +0300

    BLUE: used less if's and narrowed the code to a minimum for better work and easy to understand

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,21 @@ public class Main {
 		for(int i=1;i<=100;i++)
 		{
 		String Answer = FizzBuzz(i);
-		if(Answer.equals("FizzBuzz")) //if the number is divided by 3 and 5
-			System.out.println(Answer);
-		else if(Answer.equals("Fizz")) //if the number is divided by 3
-			System.out.println(Answer);
-		else if(Answer.equals("Buzz"))
-			System.out.println(Answer); // if the number is divided by 5
-		else
-			if(Answer.equals("None"))
+			if(Answer.equals(""))
 			       System.out.println(i);
+			else
+				 System.out.println(Answer);
 		}
 	}
 
 	
 	static String FizzBuzz(int n) {
-		if(n%3 ==0 && n%5==0)
-			return "FizzBuzz";
+		String Result= "";
 		if (n%3 == 0)
-			return "Fizz";
-		else if (n%5 == 0)
-			return "Buzz";
-		else
-			return "None";
+			Result = Result + "Fizz";//if the number is divided by 3
+		if (n%5 == 0)                        
+			Result =Result + "Buzz"; // if the number is divided by 5
+		return Result;        //if the number is divided by 3 and 5 it will add fizz and buzz 
 	}
 
 	
