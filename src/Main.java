f81b0c0763d8ae8f25243b0f2253d43433641f1f
raneemib <ramenib@gmail.com>
2014-04-23 17:11:18 +0300

    GREEN: now fizzbuzz shows for numbers that are divided by both 5 and 3

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,9 @@ public class Main {
 
 	
 	static String FizzBuzz(int n) {
-		if(n%3 == 0)
+		if(n%3 ==0 && n%5==0)
+			return "FizzBuzz";
+		if (n%3 == 0)
 			return "Fizz";
 		else if (n%5 == 0)
 			return "Buzz";
