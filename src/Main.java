672c39eb4336302f1f66fb1d7d0204832703a9e4
raneemib <ramenib@gmail.com>
2014-04-23 16:59:16 +0300

    RED: FizzBuzz from 1~>100 doesnt copies the numbers without fizz's or buzz's

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main (String[] args) 
 	{
-		int Answer = FizzBuzz(1);
+		for(int i=1;i<=100;i++)
+		{
+		int Answer = FizzBuzz(i);
 	       System.out.println(Answer);
+		}
 	}
 
 	
