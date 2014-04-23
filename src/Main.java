5ae7d24ab575431c5559485e042a02a5bcd3fbaa
raneemib <ramenib@gmail.com>
2014-04-23 17:10:13 +0300

    RED: FizzBuzz isnt shown for numbers that are divided by 5 and 3 at the same time

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,15 @@ public class Main {
 		for(int i=1;i<=100;i++)
 		{
 		String Answer = FizzBuzz(i);
-		if(Answer.equals("None"))
-	       System.out.println(i);
+		if(Answer.equals("FizzBuzz")) //if the number is divided by 3 and 5
+			System.out.println(Answer);
 		else if(Answer.equals("Fizz")) //if the number is divided by 3
 			System.out.println(Answer);
 		else if(Answer.equals("Buzz"))
 			System.out.println(Answer); // if the number is divided by 5
+		else
+			if(Answer.equals("None"))
+			       System.out.println(i);
 		}
 	}
 
